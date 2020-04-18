e5ab732326ab91b3c486b20120a7db91100efd17
kemadu <53985232+kemadu@users.noreply.github.com>
2020-04-18 12:59:36 +0100

    changes

--- config.js
+++ config.js
@@ -1,48 +1,51 @@
 'use strict';
 
 module.exports = {
-  url: 'https://lumen.netlify.com',
-  pathPrefix: '/',
-  title: 'Blog by Talent Roy',
-  subtitle: 'Freelance Journalist, Full Stack Web Developer, Social Actitvist, CEO and Historian.',
-  copyright: '© All rights reserved.',
-  disqusShortname: 'personal-blog-BaBIDhQl4L',
-  postsPerPage: 4,
-  googleAnalyticsId: 'UA-73379983-2',
-  useKatex: false,
-  menu: [{
-      label: 'Articles',
-      path: '/'
-    },
-    {
-      label: 'About me',
-      path: '/pages/about'
-    },
-    {
-      label: 'Contact me',
-      path: '/pages/contacts'
-    }
-  ],
-  author: {
-    name: 'Talent Roy',
-    photo: '/photo.jpg',
-    bio: 'Freelance Journalist, Full Stack Web Developer, Social Actitvist and Historian',
-    contacts: {
-      email: '',
-      facebook: '#',
-      telegram: '#',
-      twitter: '#',
-      github: '#',
-      rss: '',
-      vkontakte: '',
-      linkedin: '#',
-      instagram: '#',
-      line: '',
-      gitlab: '',
-      weibo: '',
-      codepen: '',
-      youtube: '',
-      soundcloud: '',
-    }
-  }
+	url: 'https://lumen.netlify.com',
+	pathPrefix: '/',
+	title: 'Blog by Talent Roy',
+	subtitle:
+		'Freelance Journalist, Full Stack Web Developer, Social Actitvist, CEO and Historian.',
+	copyright: '© All rights reserved.',
+	disqusShortname: 'personal-blog-BaBIDhQl4L',
+	postsPerPage: 4,
+	googleAnalyticsId: 'UA-73379983-2',
+	useKatex: false,
+	menu: [
+		{
+			label: 'Articles',
+			path: '/',
+		},
+		{
+			label: 'About me',
+			path: '/pages/about',
+		},
+		{
+			label: 'Contact me',
+			path: '/pages/contacts',
+		},
+	],
+	author: {
+		name: 'Talent Roy',
+		photo: '/photo.jpg',
+		bio:
+			'Freelance Journalist, Full Stack Web Developer, Social Actitvist and Historian',
+		contacts: {
+			email: '',
+			facebook: '#',
+			telegram: '#',
+			twitter: '#',
+			github: '#',
+			rss: '',
+			vkontakte: '',
+			linkedin: '#',
+			instagram: '#',
+			line: '',
+			gitlab: '',
+			weibo: '',
+			codepen: '',
+			youtube: '',
+			soundcloud: '',
+		},
+	},
 };
